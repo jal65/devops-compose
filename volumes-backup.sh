1c6c37ff3b6e866572b3fb72c47c54873b24fa9e
Hidetake Iwata <int128@gmail.com>
2016-10-26 16:08:14 +0000

    [WIP]Add JIRA backup

--- volumes-backup.sh
+++ volumes-backup.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -xe
-docker-compose exec db pg_dumpall -U postgres > backup-db.sql
-docker-compose exec ldap slapcat > backup-ldap.ldif
-docker-compose exec ldap slapcat -b cn=config > backup-ldap-config.ldif
+PREFIX=.
 
-## TODO: jira
+docker-compose exec db pg_dumpall -U postgres > $PREFIX/backup-db.sql
+docker-compose exec ldap slapcat > $PREFIX/backup-ldap.ldif
+docker-compose exec ldap slapcat -b cn=config > $PREFIX/backup-ldap-config.ldif
+
+docker-compose run -T --rm jira tar cf - . > $PREFIX/backup-jira.tar
+
 ## TODO: confluence
 ## TODO: GitBucket
 ## TODO: Jenkins
