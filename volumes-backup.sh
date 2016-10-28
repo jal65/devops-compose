d178a059eb95b81fc63e552de5ea1c33c7ba478f
Hidetake Iwata <int128@gmail.com>
2016-10-29 00:35:31 +0900

    Update volumes-backup.sh

--- volumes-backup.sh
+++ volumes-backup.sh
@@ -5,10 +5,10 @@ docker-compose stop
 
 docker-compose run -T --rm jira tar cf - . > $PREFIX/backup-jira.tar
 docker-compose run -T --rm confluence tar cf - . > $PREFIX/backup-confluence.tar
-docker-compose run -T --rm gitbucket -w /gitbucket tar cf - . > $PREFIX/backup-gitbucket.tar
-docker-compose run -T --rm jenkins -w /var/jenkins_home tar cf - . > $PREFIX/backup-jenkins.tar
-docker-compose run -T --rm mattermost -w /mattermost/data tar cf - . > $PREFIX/backup-mattermost-data.tar
-docker-compose run -T --rm mattermost -w /mattermost/config tar cf - . > $PREFIX/backup-mattermost-config.tar
+docker-compose run -T --rm -w /gitbucket gitbucket tar cf - . > $PREFIX/backup-gitbucket.tar
+docker-compose run -T --rm -w /var/jenkins_home jenkins tar cf - . > $PREFIX/backup-jenkins.tar
+docker-compose run -T --rm -w /mattermost/data mattermost tar cf - . > $PREFIX/backup-mattermost-data.tar
+docker-compose run -T --rm -w /mattermost/config mattermost tar cf - . > $PREFIX/backup-mattermost-config.tar
 
 docker-compose exec db pg_dumpall -U postgres > $PREFIX/backup-db.sql
 docker-compose exec ldap slapcat > $PREFIX/backup-ldap.ldif
